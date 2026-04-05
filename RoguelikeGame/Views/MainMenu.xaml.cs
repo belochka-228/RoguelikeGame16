ebceba8fc0aedc5c60d82277d09e592d9198ae0b
belochka-228 <sofikrohmal@gmail.com>
2026-04-05 23:09:16 +0300

    на доработку

--- RoguelikeGame/Views/MainMenu.xaml.cs
+++ RoguelikeGame/Views/MainMenu.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Shapes;
 
 namespace RoguelikeGame.Views
 {
-    /// <summary>
+    /// <summary> 
     /// Логика взаимодействия для MainMenu.xaml
     /// </summary>
     public partial class MainMenu : Window
